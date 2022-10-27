36ba000342ec98d3fd00e67d705d926b371c4a96
evanwufrog-jump <116334232+evanwufrog-jump@users.noreply.github.com>
2022-10-27 17:35:25 +0800

    main project push

---
+++ src/test/java/tw/com/de/librarysystem/LibrarysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.de.librarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarysystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
