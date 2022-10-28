bea87afa48e55a2a1feb72f4e45e0c0281419197
zongchenglee <104957908+zongchenglee@users.noreply.github.com>
2022-10-28 10:08:31 +0800

    修改至可以run

--- src/main/java/tw/com/de/librarysystem/repository/RecordDao.java
+++ src/main/java/tw/com/de/librarysystem/repository/RecordDao.java
@@ -1,6 +1,7 @@
 package tw.com.de.librarysystem.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import tw.com.de.librarysystem.entity.impl.Record;
 @Repository
 public interface RecordDao extends JpaRepository<Record, Integer>{
 
